f71d155ca5f255bfb705a8b93b6b13a8fb82ba90
laveha <vdb12122005@gmail.com>
2024-03-31 00:14:28 +0300

    hghghghg

---
+++ ClientsWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Practica1para
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientsWindow.xaml
+    /// </summary>
+    public partial class ClientsWindow : Window
+    {
+        UchPracticaEntities db = new UchPracticaEntities();
+        public ClientsWindow()
+        {
+            InitializeComponent();
+            ClientsDgr.ItemsSource = db.Clients.ToList();
+        }
+
+        private void ClientsDgr_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var dannye = ClientsDgr.SelectedItem as Clients;
+            ClientsTbx.Text = dannye.Number;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Practica1para.TatSaloonUchTableAdapters;
+
+namespace Practica1para
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //DataSet
+        OsnovnayaTableAdapter osn = new OsnovnayaTableAdapter();
+        ClientsTableAdapter clients = new ClientsTableAdapter();
+
+        //EF
+        UchPracticaEntities db = new UchPracticaEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            //DataSet
+            OsnDgr.ItemsSource = osn.GetData();
+            ClientsCbx.ItemsSource = clients.GetData();
+
+            //EF
+            //OsnDgr.ItemsSource = db.Osnovnaya.ToList();
+            //ClientsCbx.ItemsSource = db.Clients.ToList();
+        }
+
+        private void OsnDgr_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //DataSet
+            var dannye = (OsnDgr.SelectedItem as DataRowView).Row;
+            PLaceTbx.Text = dannye[2].ToString();
+            ClientsCbx.SelectedValue = Convert.ToInt32(dannye[1]);
+
+            //EF
+            //var dannye = OsnDgr.SelectedItem as Osnovnaya;
+            //PLaceTbx.Text = dannye.PlaceTatu;
+            //ClientsCbx.SelectedItem = dannye.Clients;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ClientsWindow window = new ClientsWindow();
+            window.Show();
+        }
+    }
+}
